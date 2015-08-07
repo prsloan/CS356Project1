eacbd0f14ae1d7ea9d3a3b8c3d31382e249036f7
prsloan <prsloan@csupomona.edu>
2015-08-07 11:08:33 -0700

    Testing Out IDs

--- SimulationDriver.java
+++ SimulationDriver.java
@@ -4,7 +4,16 @@ public class SimulationDriver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Student[] students = new Student[100];
+		
+		for (Student s: students){
+			s = Student.newStudent();
+			System.out.println(s.getID());
+		}
+		
+		
+		
+		
 	}
 
 }
--- Student.java
+++ Student.java
@@ -1,5 +1,27 @@
 package CS356Project1;
 
+import java.util.UUID;
 public class Student {
 
+private String id;
+
+private Student(){
+ id = UUID.randomUUID().toString();	
+}
+
+/**
+ * Static Constructor method.  Trying this out.
+ * 
+ * @return
+ */
+public static Student newStudent(){
+	return new Student();
+}
+
+
+
+public String getID(){
+	return id;
+}
+	
 }
